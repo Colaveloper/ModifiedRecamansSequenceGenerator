d73d4d32b6b9b5e74c422e51171d8599dfcbe733
Colaveloper <63930332+Colaveloper@users.noreply.github.com>
2020-05-24 17:25:51 +0200

    Chart.js inserted

--- js.js
+++ js.js
@@ -32,6 +32,7 @@ function compute() {
     // );
     computePosition();
   }
+  300;
   console.log(/*"positions", positions.toString(),*/ "jumps", jumps.toString());
 }
 
@@ -56,6 +57,7 @@ function computePosition() {
     allert("series is finite");
   }
   positions.push(position);
+  addData(position);
   jumps.push(jump);
 }
 
@@ -75,32 +77,22 @@ function jumpDirectionRight() {
   return position <= 0;
 }
 
+function addData(data) {
+  myChart.data.forEach((dataset) => {
+    dataset.data.push(data);
+  });
+  myChart.update();
+}
+
 var ctx = document.getElementById("myChart").getContext("2d");
 var myChart = new Chart(ctx, {
-  type: "bar",
+  type: "line",
+  label: "#iterations",
   data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
     datasets: [
       {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
+        label: "#iterations",
+        data: [],
       },
     ],
   },
